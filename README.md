9c4681a7bd150daf1b22f1140fb435c9f5302d0b
Przemysław Niemiec <przemyslaw.niemiec@angrynerds.co>
2022-01-20 00:17:39 +0100

    Changed base url.

---
+++ README.md
@@ -0,0 +1,6 @@
+## Deploy to Heroku
+- Commit and push changes to project repository on GitHub
+- Push previosly commited changes to Heroku with line below:
+```sh
+git push heroku master
+```
--- src/main/kotlin/com/pro8l3m/data/mocks/Mocks.kt
+++ src/main/kotlin/com/pro8l3m/data/mocks/Mocks.kt
@@ -2,7 +2,7 @@ package com.pro8l3m.data.mocks
 
 import com.pro8l3m.data.model.User
 
-const val BASE_URL = "https://peaceful-island-34483.herokuapp.com"
+const val BASE_URL = "http://peaceful-island-34483.herokuapp.com"
 
 object Mocks {
     val users = mutableListOf(
